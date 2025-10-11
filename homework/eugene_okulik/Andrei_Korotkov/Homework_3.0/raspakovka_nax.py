61658f76de62c6249d18fd500260d529a34ef9aa
andreik88 <akorotkov1987@yandex.ru>
2025-10-11 17:42:34 +0300

    HW_na_proverky

---
+++ homework/eugene_okulik/Andrei_Korotkov/Homework_3.0/raspakovka_nax.py
@@ -0,0 +1,31 @@
+
+print('=' * 36)
+print('Задание: ', 1, sep='№')
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(name, last_name, city, phone, country, sep='|')
+
+print('=' * 36)
+print('Задание: ', 2, sep='№')
+
+chislo_1 = ('Результат работы программы:', 16)
+print(chislo_1.index(16) + chislo_1[-1])
+
+chislo_2 = ('Результат работы программы:', 656)
+print(chislo_2.index(656) + chislo_2[-1])
+
+chislo_3 = ('Результат работы программы:', 5)
+print(chislo_3.index(5) + chislo_3[-1])
+
+print('=' * 36)
+print('Задание: ', 3, sep='№')
+students = ['Ivanov', 'Petrov', 'Sidorov']
+# students = ', '.join(students)
+# print(students)
+subjects = ['math', 'biology', 'geography']
+# subjects = ', '.join(subjects)
+# print(subjects)
+students, subjects = ', '.join(students), ', '.join(subjects)
+print('Students', students, 'study these subjects:', subjects)
+print('=' * 36)
+
