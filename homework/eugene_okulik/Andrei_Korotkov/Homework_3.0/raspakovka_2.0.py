cd7bea4c4e890dd10bc60e2c0388a7266bc2d56c
andreik88 <akorotkov1987@yandex.ru>
2025-10-14 17:33:38 +0300

    Peredel code

--- homework/eugene_okulik/Andrei_Korotkov/Homework_3.0/raspakovka_2.0.py
+++ homework/eugene_okulik/Andrei_Korotkov/Homework_3.0/raspakovka_2.0.py
@@ -8,14 +8,20 @@ print(name, last_name, city, phone, country, sep='|')
 print('=' * 36)
 print('Задание: ', 2, sep='№')
 
-chislo_1 = ('Результат работы программы:', 16)
-print(chislo_1[-1] + 10)
+chislo_1 = ('Результат работы программы:', 15)
+raspokovka_1 = f"{chislo_1[0]} {chislo_1[1]}"
+search_1 = int(raspokovka_1.split()[-1])
+print(search_1 + 10)
 
-chislo_2 = ('Результат работы программы:', 656)
-print(chislo_2[-1] + 10)
+chislo_2 = ('Результат работы программы:', 343333)
+raspokovka_2 = f"{chislo_2[0]} {chislo_2[1]}"
+search_2 = int(raspokovka_2.split()[-1])
+print(search_2 + 10)
 
-chislo_3 = ('Результат работы программы:', 5)
-print(chislo_3[-1] + 10)
+chislo_3 = ('Результат работы программы:', 545)
+raspokovka_3 = f"{chislo_3[0]} {chislo_3[1]}"
+search_3 = int(raspokovka_3.split()[-1])
+print(search_3 + 10)
 
 print('=' * 43)
 print('Задание: ', 3, sep='№')
